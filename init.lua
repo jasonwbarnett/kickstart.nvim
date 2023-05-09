da0f32f4a8c0509d313e9c01809f15a5ce2bab06
Jason Barnett <J@sonBarnett.com>
2023-05-08 19:09:16 -0600

    add gopls and ruby_ls LSPs

--- init.lua
+++ init.lua
@@ -412,10 +412,12 @@ end
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
+  
+  ruby_ls = {},
 
   lua_ls = {
     Lua = {
